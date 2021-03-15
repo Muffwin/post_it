1211bdf562c1c507864dd3292874b25b1a7bb27e
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2021-03-15 16:05:44 +0100

    Premier jet, avec les data en props

---
+++ src/components/Note.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ data.title }}</h1>
+        <p>{{ data.desc }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Note',
+  props: {
+    data: Object,
+  },
+}
+</script>
+
+<style scoped>
+div {
+  width: 300px;
+  height: 200px;
+  padding: 15px;
+  background-color: yellow;
+  box-shadow: 10px 10px 5px grey;
+  display: inline-block;
+  margin: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <img alt="Vue logo" src="../assets/logo.png">
+  <div class="notes">
+    <Note :data="{title: 'Première note', desc: 'Je suis une note !'}" />
+    <Note :data="{title: 'Deuxième note', desc: 'Salut les amios'}"/>
+    <Note :data="{title: 'Troisième note', desc: 'Vuejs c\'est pas si mal en fait'}"/>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Note from '@/components/Note.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Note
   }
 }
 </script>
+
+<style scoped>
+.home {
+  display: inline-block;
+}
+</style>
